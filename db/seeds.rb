89377e6172c0a9dc69a4d3cef2f278d22aa07972
konradrogala <konrad.rogala@windowslive.com>
2024-11-16 16:21:10 +0100

    recipe model with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# require 'json'
+
+file_path = Rails.root.join('config/initializers', 'recipes_en.json')
+recipes = JSON.parse(File.read(file_path))
+recipes.each do |recipe|
+  Recipe.create!(
+    title: recipe['title'],
+    cook_time: recipe['cook_time'],
+    prep_time: recipe['prep_time'],
+    rating: recipe['ratings'] * 100,
+    cuisine: recipe['cuisine'],
+    author: recipe['author'],
+    category: recipe['category'],
+    image_url: recipe['image']
+  )
+end
+
+puts "Data imported correctly!"
