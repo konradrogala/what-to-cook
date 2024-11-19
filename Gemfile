47badae63c8bd535be2508115f0cec9daa393a2f
konradrogala <konrad.rogala@windowslive.com>
2024-11-19 10:43:23 +0100

    remove pg_search

--- Gemfile
+++ Gemfile
@@ -58,5 +58,3 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
-
-gem "pg_search"
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,15 +1,4 @@
 class Recipe < ApplicationRecord
-  include PgSearch::Model
-
-  # Zakładamy, że Recipe ma wiele składników przez RecipeIngredient
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
-
-  pg_search_scope :search_by_ingredients,
-    associated_against: {
-      ingredients: :name # Wyszukiwanie w nazwach składników
-    },
-    using: {
-      tsearch: { prefix: true } # Częściowe dopasowanie np. 'gar' znajdzie 'garlic'
-    }
 end
